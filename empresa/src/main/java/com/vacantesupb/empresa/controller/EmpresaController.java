d2629cb171d63769c4f639748acd85665fee88fa
Jonathan <yotomc17@gmail.com>
2022-10-25 21:20:43 -0500

    create repository, test with local api routes, and CRUD functions completed

--- empresa/src/main/java/com/vacantesupb/empresa/controller/EmpresaController.java
+++ empresa/src/main/java/com/vacantesupb/empresa/controller/EmpresaController.java
@@ -6,12 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +24,11 @@ public class EmpresaController {
     //Personalizar peticiones
     Map<String, Object> map = new HashMap<>();
 
+    @GetMapping
+    private ResponseEntity<ArrayList<Empresa>> getAll() {
+        return ResponseEntity.ok(empresaService.getAll());
+    }
+    
     @PostMapping
     private ResponseEntity<Object> save(@RequestBody Empresa obj) {
         try {
@@ -35,6 +39,40 @@ public class EmpresaController {
             map.put("error", true);
             map.put("mensaje", "No se pudo crear la empresa");
             return new ResponseEntity<Object>(map, HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping(path = "/{id}")
+    private ResponseEntity<Object> update(@PathVariable("id") long id, @RequestBody Empresa obj) {
+        try {
+            obj.setId(id);
+            obj = empresaService.saveOrUpdate(obj);
+            return ResponseEntity.accepted().body(obj);
+        } catch (Exception ex) {
+            map.put("error", true);
+            map.put("mensaje", "No se pudo actualizar la empresa");
+            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(path = "/{id}")
+    private ResponseEntity<Object> findById(@PathVariable("id") long id) {
+        ResponseEntity response = ResponseEntity.ok(empresaService.findById(id));
+        return response;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    private ResponseEntity<Object> deleteById(@PathVariable("id") long id) {
+        ResponseEntity response = ResponseEntity.ok(empresaService.findById(id));
+        if(response.toString().contains("Optional.empty")) {
+            map.put("error", true);
+            map.put("message", "No existe una empresa con id:" + id);
+            return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+        } else {
+            empresaService.delete(id);
+            map.put("error", false);
+            map.put("message", "La empresa fue eliminada correctamente");
+            return new ResponseEntity<>(map, HttpStatus.OK);
         }
     }
 }
