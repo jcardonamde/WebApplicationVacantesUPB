346862ebe05bf6741cd550542e4780a2dfbe86cf
jcardonamde <yotomc17@gmail.com>
2022-11-11 16:00:41 -0500

    Create access for JSON and create token generator method

--- empresa/src/main/java/com/vacantesupb/empresa/controller/UsuarioController.java
+++ empresa/src/main/java/com/vacantesupb/empresa/controller/UsuarioController.java
@@ -2,7 +2,17 @@ package com.vacantesupb.empresa.controller;
 
 import com.vacantesupb.empresa.model.Usuario;
 import com.vacantesupb.empresa.model.UsuarioSession;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class UsuarioController {
@@ -25,8 +35,10 @@ public class UsuarioController {
     @PostMapping("login")
     //Creo metodo de logueo usando la consulta de Tipo JSON
     public UsuarioSession loginToken2(@RequestBody Usuario usr) {
-        String token = "sdsdsajdkajdksjakdja";
+        //String token = "sdsdsajdkajdksjakdja";
+        
         //Creo un nuevo objeto usuario sesión
+        String token = getJWTToken(usr.getUsuario());
         String user = usr.getUsuario();
         String pwd = usr.getContrasena();
 
@@ -37,5 +49,32 @@ public class UsuarioController {
         us.setCorreo("yoto@upb.edu.co");
         us.setToken(token);
         return us;
+    }
+
+    private String getJWTToken(String usuario) {
+        // Se define el tiempo que va durar la sesión
+        int minutos = 1;
+        int duracion = minutos * 60 * 1000;
+        String claveSecreta = "myClave1900upb2112";
+
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                //Libreria que permite generar rol anonimo para accesos
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token = Jwts
+                .builder()
+                .setId("myId4578966413555")
+                .setSubject(usuario)
+                .claim("authorities",
+                        grantedAuthorities.stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList())
+                )
+                //Fecha cuando se creo y expira el Token
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+duracion))
+                .signWith(SignatureAlgorithm.HS512,
+                        claveSecreta.getBytes()).compact();
+        return token;
     }
 }
